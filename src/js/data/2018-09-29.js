a884e5b7aebff40cb8488ad5bfad3532ab091438
So4nk <45759744+So4nk@users.noreply.github.com>
2018-12-10 13:54:57 +0100

    Add files via upload

---
+++ src/js/data/2018-09-29.js
@@ -0,0 +1,461 @@
+dataSetVersion = "2018-09-29"; // Change this when creating a new data set version. YYYY-MM-DD format.
+
+dataSet[dataSetVersion] = {};
+
+
+dataSet[dataSetVersion].options = [
+  
+{
+    name: "Filter by Series Entry",
+
+    key: "series",
+
+    tooltip: "Check this to restrict to certain series.",
+
+    checked: false,
+
+    sub: [
+
+      { name: "Pretty rhythm Rainbow live", key: "rainbow" },
+
+      { name: "King of prism", key: "kinpri" }
+
+    ]
+  },
+  {
+    name: "Remove Duplicates",
+    key: "dupe",
+    tooltip: "Check this to remove duplicates (including the Madarai brothers, except Isshiki)."
+  }
+];
+dataSet[dataSetVersion].characterData = [
+
+  {
+
+    name: "Ayase Naru",
+
+    img: "261085.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Fukuhara Ann",
+
+    img: "286345.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Ibara Rinne",
+
+    img: "286348.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Morizono Wakana",
+
+    img: "286342.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Renjouji Bell",
+
+    img: "286346.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Suzuno Ito",
+
+    img: "283269.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Takanashi Otoha",
+
+    img: "286341.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Amou June",
+
+    img: "298859.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Himuro Hijiri",
+
+    img: "331976.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Momo",
+
+    img: "283264.jpg",
+
+    opts: {
+
+      series: ["rainbow"]
+
+      }
+  
+},
+
+{
+
+    name: "Kurokawa Rei",
+
+    img: "331977.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Mihama Kouji",
+
+    img: "304991.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Nishina Kazuki",
+
+    img: "304990.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Hiro Hayami",
+
+    img: "304992.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Norizuki Jin",
+
+    img: "331978.jpg",
+
+    opts: {
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Suzuno Yuu",
+
+    img: "331957.jpg",
+
+    opts: {
+
+      series: ["rainbow", "kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Ichijou Shin",
+
+    img: "315534.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Kisaragi Louis",
+
+    img: "301727.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Juuouin Kakeru",
+
+    img: "331955.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Kougami Taiga",
+
+    img: "331954.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Saionji Leo",
+
+    img: "331956.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Tachibana Yukinojou",
+
+    img: "331958.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Takahashi Minato",
+
+    img: "331959.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Yamada Ryou",
+
+    img: "331974.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Ace",
+
+    img: "ACE.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Kokoro Gotanda",
+
+    img: "Kokoro Gotanda.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Mitsuba Kanda",
+
+    img: "Mitsuba Kanda.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Tsurugi Okachimachi",
+
+    img: "Tsurugi Okachimachi.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+
+{
+
+    name: "Mondo Uguisudani",
+
+    img: "Mondo Uguisudani.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+{
+
+    name: "Yamato Alexander",
+
+    img: "349659.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+
+      }
+  
+},
+{
+
+    name: "Takadanobaba Joji",
+
+    img: "350000.jpg",
+
+    opts: {
+
+      series: ["kinpri"]
+    }
+  }
+];
